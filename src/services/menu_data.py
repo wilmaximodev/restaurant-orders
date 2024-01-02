fde57cf0a33a6c689d4f383b3d648ae71b3b2728
Wilson Maximo <wilson-max@hotmail.com>
2024-01-02 14:11:21 -0300

    Req 3

--- src/services/menu_data.py
+++ src/services/menu_data.py
@@ -1,4 +1,33 @@
-# Req 3
+import csv
+from models.dish import Dish
+from models.ingredient import Ingredient
+
 class MenuData:
     def __init__(self, source_path: str) -> None:
-        pass
+        self.dishes = set()
+        self._menu_data(source_path)
+
+    def _menu_data(self, source_path: str) -> None:
+        with open(source_path) as csv_file:
+            reader = csv.reader(csv_file)
+            
+            next(reader)
+            
+            dishes_data = {}
+
+            for row in reader:
+                dish_name = row[0]
+                price_str = row[1]
+                ingredient_name = row[2]
+                quantity_str = row[3]
+                
+                price = float(price_str)
+                quantity = int(quantity_str)
+                dish = dishes_data.get(dish_name)
+                if not dish:
+                    dish = Dish(dish_name, price)
+                    self.dishes.add(dish)
+                    dishes_data[dish_name] = dish
+
+                ingredient = Ingredient(ingredient_name)
+                dish.add_ingredient_dependency(ingredient, quantity)
